0a0d280463398a1da6352200148ad07148e8aa3f
Sophie <worteljulian2@gmail.com>
2026-09-18 16:06:51 +0200

    new tables alert!!
    holy moley

--- db/db.sql
+++ db/db.sql
@@ -1,6 +1,24 @@
+-- Table for users
 CREATE TABLE user(
+	userID INT PRIMARY KEY,
 	user TEXT,
 	display TEXT,
 	pass TEXT,
 	groups TEXT
 );
+
+-- Table for groups
+CREATE TABLE groups(
+	groupID INT PRIMARY KEY,
+	owner INT,
+	name TEXT,
+	desc TEXT,
+	members TEXT,
+	picture TEXT,
+	blacklist TEXT
+);
+
+CREATE TABLE message(
+	by INT,
+	cont TEXT
+); -- For storing group messages
