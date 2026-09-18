125acb41a1cdf58fb91b51d5299dc0af972d367a
Hedgwhog <hedgwhog@gmail.com>
2026-09-18 15:45:06 +0200

    New patchnotes + more
    New patchnotes from Sophie
    Added links to pages on the 4 big buttons
    Added background image to homepage
    Reworked notifications slightly in design

--- home.php
+++ home.php
@@ -23,6 +23,8 @@
                 <p>Fixed homepage responsiveness text </p>
                 <p>Added upload (Frontend)</p>
                 <p>Reworked cool static effect (a little)</p>
+                <p>Added homepage background</p>
+                <p>Added account (backend)</p>
                 <h3>[11-09-2026]</h3>
                 <p>Added login & signup (backend)</p>
                 <p>Changed "index.html" -> "home.php"</p>
@@ -37,30 +39,36 @@
 
             <div class="homepage-camera-top" style="color: red;">Test Text (will get deleted soon: <b><?= $_SESSION['user'] ?></b></div>
 
-            <div class="homepage-camera-button-account">
-                <div class="homepage-camera-button-account-title">Account</div>
-            </div>
-
-            <div class="homepage-camera-button-groups">
-                <div class="homepage-camera-button-groups-title">Groups</div>
-                <button onclick="homepageAnnouncements.style.display = 'block'" class="homepage-camera-button-groups-red-button">
-                    <div style="color: white; font-size: clamp(0.01em, 3.5cqmin, 1.8rem);">2</div>
-                    <!-- The number needs later to be connected to the announcements. The amount of announcements you get is the amount that needs to be shown! -->
-                </button>
-
-                <div id="homepageAnnouncements">
-                    <button class="homepageAnnouncements-close" onclick="homepageAnnouncements.style.display = 'none'" style="width: 14%; height: 10%;">Close</button>
-                    <h2>Announcements</h2>
-                    <!-- In announcements their needs to be the following 3 possibilites that can happen
-                     1. They get a groupchat invite, which they can accept or decline
-                     2. They get an announcement that they have been removed from [groupchatname] 
-                     3. We should somehow be able to announce something to all users-->
+            <a href="accsettings.php" class="homepage-camera-button-upload-link">
+                <div class="homepage-camera-button-account">
+                    <div class="homepage-camera-button-account-title">Account</div>
                 </div>
-            </div>
+            </a>
 
-            <div class="homepage-camera-button-settings">
-                <div class="homepage-camera-button-settings-title">Setings</div>
-            </div>
+            <a href="groups.php" class="homepage-camera-button-upload-link">
+                <div class="homepage-camera-button-groups">
+                    <div class="homepage-camera-button-groups-title">Groups</div>
+                    <button onclick="homepageAnnouncements.style.display = 'block'" class="homepage-camera-button-groups-red-button">
+                        <div style="color: white; font-size: clamp(0.01em, 3.5cqmin, 1.8rem);">2</div>
+                        <!-- The number needs later to be connected to the announcements. The amount of announcements you get is the amount that needs to be shown! -->
+                    </button>
+
+                    <div id="homepageAnnouncements">
+                        <button class="homepageAnnouncements-close" onclick="homepageAnnouncements.style.display = 'none'" style="width: 14%; height: 10%;">+</button>
+                        <h2>Announcements</h2>
+                        <!-- In announcements their needs to be the following 3 possibilites that can happen
+                        1. They get a groupchat invite, which they can accept or decline
+                        2. They get an announcement that they have been removed from [groupchatname] 
+                        3. We should somehow be able to announce something to all users-->
+                    </div>
+                </div>
+            </a>
+
+            <a href="settings.php" class="homepage-camera-button-upload-link">
+                <div class="homepage-camera-button-settings">
+                    <div class="homepage-camera-button-settings-title">Setings</div>
+                </div>
+            </a>
 
             <a href="upload.php" class="homepage-camera-button-upload-link">
                 <div class="homepage-camera-button-upload">
